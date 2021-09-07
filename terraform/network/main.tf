b21de0351fa49744e6e46bc7036b3bbde1d1ac13
MLeongQA <MLeong@qa.com>
2021-09-07 09:13:05 +0000

    Removed cluster subnet

--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -4,13 +4,6 @@ resource "azurerm_virtual_network" "main" {
     location            = var.location
     resource_group_name = var.group_name
 }
-
-resource "azurerm_subnet" "cluster" {
-    name                 = "subnet-cluster"
-    resource_group_name  = var.group_name
-    virtual_network_name = azurerm_virtual_network.main.name
-    address_prefixes     = ["10.0.2.0/24"]
-}
 resource "azurerm_subnet" "jumpbox" {
     name                 = "subnet-jumpbox"
     resource_group_name  = var.group_name
@@ -22,18 +15,6 @@ resource "azurerm_subnet" "jenkins" {
     resource_group_name  = var.group_name
     virtual_network_name = azurerm_virtual_network.main.name
     address_prefixes     = ["10.0.4.0/24"]
-}
-
-resource "azurerm_network_interface" "cluster" {
-    name                = "cluster-nic"
-    location            = var.location
-    resource_group_name = var.group_name
-
-    ip_configuration {
-        name                          = "internal"
-        subnet_id                     = azurerm_subnet.cluster.id
-        private_ip_address_allocation = "Dynamic"
-    }
 }
 
 resource "azurerm_network_interface" "jumpbox" {
@@ -58,29 +39,6 @@ resource "azurerm_network_interface" "jenkins" {
         subnet_id                     = azurerm_subnet.jenkins.id
         private_ip_address_allocation = "Dynamic"
     }
-}
-
-resource "azurerm_network_security_group" "cluster" {
-  name                = "cluster-nsg"
-  location            = var.location
-  resource_group_name = var.group_name
-
-  security_rule {
-    name                       = "cluster-rule"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_subnet_network_security_group_association" "cluster" {
-  subnet_id                 = azurerm_subnet.cluster.id
-  network_security_group_id = azurerm_network_security_group.cluster.id
 }
 
 resource "azurerm_network_security_group" "jumpbox" {
